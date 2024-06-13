522aea9a70f80ef58142b6db1e787c2059ac3676
mccarthurmay <mccarthurmay@gmail.com>
2024-06-13 16:12:36 -0400

    update toggle_hdr - located correct registry path to toggle HDR - use nircmd to refresh monitor

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import subprocess
 
+def run_command(command):
+    subprocess.run(command, shell=True)
+
+
 def toggle_hdr(enable):
     if enable:
-        command = "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\VideoSettings' -Name 'EnableHDR' -Value 1"
+        command = r"powershell -Command Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\MonitorDataStore\SDC416D0_19_07E5_72' -Name 'AdvancedColorEnabled' -Value 1"
+        print('1')
+        run_command(command)
+        nircmd_path = r'C:\Users\mccar\OneDrive\Desktop\toggle_settings\nircmd\nircmd.exe'
+        run_command(f'{nircmd_path} monitor off')
+        run_command(f'{nircmd_path} monitor on')
     else:
-        command = "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\VideoSettings' -Name 'EnableHDR' -Value 0
-    
-    subprocess.run(command, shell = True)
+        command = r"powershell -Command Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\MonitorDataStore\SDC416D0_19_07E5_72' -Name 'AdvancedColorEnabled' -Value 0"
+        run_command(command)       
+        nircmd_path = r'C:\Users\mccar\OneDrive\Desktop\toggle_settings\nircmd\nircmd.exe'
+        run_command(f'{nircmd_path} monitor off')
+        run_command(f'{nircmd_path} monitor on')    
+
+toggle_hdr(False)
 
 
-command = "Restart-Service -Name 'DisplayEnhancementService'"
 
 def hertz(enable):
     if enable:
@@ -44,6 +56,7 @@ def video():
     #120HZ
     #turn up quality in battery
     #whisper fan
+    pass
 
 def education():
     #HDR off
@@ -51,15 +64,18 @@ def education():
     #turn down quality in battery
     #whisper fan
     #turn on target mode in asus settings
+    pass
 
 def quality():
     #HDR on
     #120HZ
     #turn up quality in battery
     #standard fan
+    pass
 
 def performance():
     #HDR off
     #60HZ
     #turn down quality in battery
     #performance fan
+    pass    
